761040d52ee0cfe5a938a3519e2e391a8b94b273
doanvpt-imt <doanvpt@imt-soft.com>
2023-12-06 11:17:12 +0700

    create i18n

--- Example-demo/src/app/app.component.ts
+++ Example-demo/src/app/app.component.ts
@@ -1,10 +1,28 @@
 import { Component, OnInit } from '@angular/core';
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
 })
 export class AppComponent implements OnInit {
+  minutes = 0;
+  gender = 'female';
+  fly = true;
+  logo = 'https://angular.io/assets/images/logos/angular/angular.png';
+  inc(i: number) {
+    this.minutes = Math.min(5, Math.max(0, this.minutes + i));
+  }
+  male() {
+    this.gender = 'male';
+  }
+  female() {
+    this.gender = 'female';
+  }
+  other() {
+    this.gender = 'other';
+  }
+
   ngOnInit() {}
 
   // Input - Output
---
+++ Internationalization/src/app/app.component.ts
@@ -0,0 +1,19 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { RouterOutlet } from '@angular/router';
+import { Title } from '@angular/platform-browser';
+
+@Component({
+  selector: 'app-root',
+  standalone: true,
+  imports: [CommonModule, RouterOutlet],
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.scss',
+})
+export class AppComponent {
+  title = 'Internationalization';
+
+  constructor(private titleService: Title) {
+    this.titleService.setTitle($localize`${this.title}`);
+  }
+}
